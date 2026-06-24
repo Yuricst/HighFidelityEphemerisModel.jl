e98d733b0baa9825a97beb2dee2a0925eae59a0d
Derek Phung <phungd3@uci.edu>
2026-06-24 01:43:03 -0700

    Add Ephemerides N-body EOM

--- src/HighFidelityEphemerisModel.jl
+++ src/HighFidelityEphemerisModel.jl
@@ -27,6 +27,7 @@ include("jacobians_sparsediff.jl")
 
 include("eoms/eom_Nbody_SPICE.jl")
 include("eoms/eom_Nbody_Interp.jl")
+include("eoms/eom_Nbody_Ephemerides.jl")
 
 include("eoms/eom_NbodySH_SPICE.jl")
 include("eoms/eom_NbodySH_Interp.jl")
@@ -49,6 +50,7 @@ export HighFidelityEphemerisModelParameters
 
 export eom_Nbody_SPICE!, eom_Nbody_SPICE, eom_stm_Nbody_SPICE!, eom_stm_Nbody_SPICE_fd!
 export eom_Nbody_Interp!, eom_Nbody_Interp, dfdx_Nbody_Interp, eom_stm_Nbody_Interp!, eom_stm_Nbody_Interp_fd!
+export eom_Nbody_Ephemerides!, eom_Nbody_Ephemerides, dfdx_Nbody_Ephemerides, eom_stm_Nbody_Ephemerides!, eom_stm_Nbody_Ephemerides_fd!
 
 export eom_NbodySH_SPICE!, eom_NbodySH_SPICE, eom_stm_NbodySH_SPICE_fd!
 export eom_NbodySH_Interp!, eom_NbodySH_Interp, eom_stm_NbodySH_Interp_fd!
---
+++ src/eoms/eom_Nbody_Ephemerides.jl
@@ -0,0 +1,172 @@
+"""Ephemerides.jl-based N-body equations of motion"""
+
+
+function _get_pos_3body_ephemerides(params, ID::String, t)
+    return collect(
+        get_pos_ephemerides(
+            params.ephemerides_provider,
+            ID,
+            params.naif_ids[1],
+            params.et0 + t*params.TU,
+        )
+    ) / params.DU
+end
+
+
+"""
+    eom_Nbody_Ephemerides!(dx, x, params, t)
+
+Right-hand side of N-body equations of motion compatible with `DifferentialEquations.jl`
+"""
+function eom_Nbody_Ephemerides!(dx, x, params, t)
+    if params.include_drag
+        error("eom_Nbody_Ephemerides! does not currently support drag.")
+    end
+
+    dx[1:3] = x[4:6]
+    dx[4:6] = -params.mus[1] / norm(x[1:3])^3 * x[1:3]
+
+    for (i,(ID,mu_i)) in enumerate(zip(params.naif_ids, params.mus))
+        if i == 1
+            pos_3body = [0.0, 0.0, 0.0]   # needed in case Sun is central body (first body) & we need SRP
+        else
+            pos_3body = _get_pos_3body_ephemerides(params, ID, t)
+        end
+
+        if i >= 2
+            dx[4:6] += third_body_accel(x[1:3], pos_3body, mu_i)
+        end
+
+        if ID == "10" && params.include_srp
+            dx[4:6] += srp_cannonball(x[1:3], pos_3body, params.k_srp_cannonball)
+        end
+    end
+
+    return nothing
+end
+
+
+"""
+    eom_Nbody_Ephemerides(x, params, t)
+
+Right-hand side of N-body equations of motion compatible with `DifferentialEquations.jl`
+"""
+function eom_Nbody_Ephemerides(x, params, t)
+    if params.include_drag
+        error("eom_Nbody_Ephemerides does not currently support drag.")
+    end
+
+    dx = [x[4:6]; -params.mus[1] / norm(x[1:3])^3 * x[1:3]]
+
+    for (i,(ID,mu_i)) in enumerate(zip(params.naif_ids, params.mus))
+        if i == 1
+            pos_3body = [0.0, 0.0, 0.0]   # needed in case Sun is central body (first body) & we need SRP
+        else
+            pos_3body = _get_pos_3body_ephemerides(params, ID, t)
+        end
+
+        if i >= 2
+            dx[4:6] += third_body_accel(x[1:3], pos_3body, mu_i)
+        end
+
+        if ID == "10" && params.include_srp
+            dx[4:6] += srp_cannonball(x[1:3], pos_3body, params.k_srp_cannonball)
+        end
+    end
+
+    return dx
+end
+
+
+"""
+    eom_stm_Nbody_Ephemerides!(dx_stm, x_stm, params, t)
+
+Right-hand side of N-body equations of motion with STM compatible with `DifferentialEquations.jl`
+"""
+function eom_stm_Nbody_Ephemerides!(dx_stm, x_stm, params, t)
+    if params.include_drag
+        error("eom_stm_Nbody_Ephemerides! does not currently support drag.")
+    end
+
+    dx_stm[1:3] = x_stm[4:6]
+    dx_stm[4:6] = -params.mus[1] / norm(x_stm[1:3])^3 * x_stm[1:3]
+    Rs = similar(params.Rs)
+    R_sun = zeros(eltype(params.R_sun), length(params.R_sun))
+
+    for (i,(ID,mu_i)) in enumerate(zip(params.naif_ids, params.mus))
+        if i == 1
+            pos_3body = [0.0, 0.0, 0.0]   # needed in case Sun is central body (first body) & we need SRP
+        else
+            pos_3body = _get_pos_3body_ephemerides(params, ID, t)
+        end
+
+        if i >= 2
+            Rs[1+3(i-2):3(i-1)] = pos_3body
+            dx_stm[4:6] += third_body_accel(x_stm[1:3], pos_3body, params.mus[i])
+        end
+
+        if ID == "10" && params.include_srp
+            R_sun .= pos_3body
+            dx_stm[4:6] += srp_cannonball(x_stm[1:3], pos_3body, params.k_srp_cannonball)
+        end
+    end
+
+    if params.include_srp
+        A = params.f_jacobian(x_stm[1:6], params.mus, Rs, params.k_srp_cannonball, R_sun)
+    else
+        A = params.f_jacobian(x_stm[1:6], params.mus, Rs)
+    end
+    dx_stm[7:42] = reshape((A * reshape(x_stm[7:42],6,6)), 36)
+    return nothing
+end
+
+
+"""
+    dfdx_Nbody_Ephemerides(x, u, params, t)
+
+Evaluate Jacobian of N-body problem
+"""
+function dfdx_Nbody_Ephemerides(x, u, params, t)
+    if params.include_drag
+        error("dfdx_Nbody_Ephemerides does not currently support drag.")
+    end
+
+    Rs = similar(params.Rs)
+    R_sun = zeros(eltype(params.R_sun), length(params.R_sun))
+
+    for (i,(ID,mu_i)) in enumerate(zip(params.naif_ids, params.mus))
+        if i == 1
+            pos_3body = [0.0, 0.0, 0.0]   # needed in case Sun is central body (first body) & we need SRP
+        else
+            pos_3body = _get_pos_3body_ephemerides(params, ID, t)
+        end
+
+        if i >= 2
+            Rs[1+3(i-2):3(i-1)] = pos_3body
+        end
+
+        if ID == "10" && params.include_srp
+            R_sun .= pos_3body
+        end
+    end
+
+    if params.include_srp
+        return params.f_jacobian(x[1:6], params.mus, Rs, params.k_srp_cannonball, R_sun)
+    else
+        return params.f_jacobian(x[1:6], params.mus, Rs)
+    end
+end
+
+
+"""
+    eom_stm_Nbody_Ephemerides_fd!(dx_stm, x_stm, params, t)
+
+Right-hand side of N-body equations of motion with STM compatible with `DifferentialEquations.jl`
+"""
+function eom_stm_Nbody_Ephemerides_fd!(dx_stm, x_stm, params, t)
+    dx_stm[1:6] = eom_Nbody_Ephemerides(x_stm[1:6], params, t)
+    A = eom_jacobian_fd(eom_Nbody_Ephemerides, x_stm[1:6], 0.0, params, t)
+    A[1:3,4:6] .= I(3)   # force identity for linear map
+    dx_stm[7:42] = reshape((A * reshape(x_stm[7:42],6,6)), 36)
+    return nothing
+end
